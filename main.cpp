b191b91da7793ca57b9e255ebe8772f4128ff7fa
rudik <rudik.login@gmail.com>
2024-05-13 13:34:39 +0300

    add some changes

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@ int main(int, char const **) {
     print_ip(int32_t{2130706433});
     print_ip(int64_t{8875824491850138409} );
     print_ip(std::string{"Hello, World!"});
-    print_ip(std::vector<int>{100, 200, 300, 400});
-    print_ip(std::list<short>{400, 300, 200, 100});
+    print_ip<std::vector<int>>(std::vector<int>{100, 200, 300, 400});
+    print_ip<std::list<short>>(std::list<short>{400, 300, 200, 100});
     print_ip(std::make_tuple(123, 456, 789, 0));
     return 0;
 }
--- print_ip.hpp
+++ print_ip.hpp
@@ -3,7 +3,7 @@
 
 // Вызываем если параметр от short до long long
 template <typename T>
-static std::enable_if_t<std::is_integral_v<T>, void>
+static std::enable_if_t<std::is_integral_v<T>>
 print_ip(T value)
 {
     static const size_t constexpr size = sizeof(T);
@@ -18,14 +18,59 @@ print_ip(T value)
 
 // Вызываем если std::string
 template<typename T>
-static std::enable_if_t<std::is_same_v<T, std::string>, void>
+static std::enable_if_t<std::is_same_v<T, std::string>>
 print_ip(T value){
     std::cout << value << std::endl;
 }
 
 // Вызываем если параметр std::vector или std::list
-template<template <typename> typename Container, typename Type>
-static std::enable_if_t<std::is_same_v<Container<Type>, std::vector<Type>>, void>
-print_ip(Container<Type>){
-    std::cout << "test" << std::endl;
+template <typename>
+struct is_std_vector_or_list : std::false_type {};
+
+template <typename T, typename... Args>
+struct is_std_vector_or_list<std::vector<T, Args...>> : std::true_type {};
+
+template <typename T, typename... Args>
+struct is_std_vector_or_list<std::list<T, Args...>> : std::true_type {};
+
+template <typename T>
+static void print_ip(std::enable_if_t<is_std_vector_or_list<T>::value, const T&> container)
+{
+    for (auto it = container.begin(); it != container.end(); ++it)
+    {
+        std::cout << *it;
+        if (it != std::prev(container.end()))
+            std::cout << '.';
+    }
+    std::cout << std::endl;
+}
+
+// Вызываем если std::tuple
+template <size_t I>
+struct PrintTuple
+{
+    template <typename Type0, typename... Types>
+    static constexpr void Print(const std::tuple<Type0, Types...>& tuple)
+    {
+        auto number = std::get<sizeof...(Types) + 1 - I>(tuple);
+        std::cout << number;
+        if (I > 1)
+            std::cout << '.';
+
+        PrintTuple<I - 1>::Print(tuple);
+    }
+};
+
+template <>
+struct PrintTuple<0>
+{
+    template <typename... Types>
+    static constexpr void Print(const std::tuple<Types...>&) {}
+};
+
+template <typename... Types>
+static void print_ip(const std::tuple<Types...>& tuple)
+{
+    PrintTuple<sizeof...(Types)>::Print(tuple);
+    std::cout << std::endl;
 }
